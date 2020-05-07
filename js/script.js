d89f23bdeb0723ad489a8168fcd1c7e8d474e83f
Roy Nyaga <nyagaandreroy@gmail.com>
2020-05-08 00:15:46 +0100

    add magnific popup to our work section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $('.parent-container').magnificPopup({
+  $('.image-container').magnificPopup({
 	  delegate: 'a', // child items selector, by clicking on it popup will open
 	  type: 'image',
 	  gallery: {
