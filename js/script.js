623b0db02e486f3e713e6c27f50d7d642825b36d
Roy Nyaga <nyagaandreroy@gmail.com>
2020-05-11 07:12:57 +0100

    add our customers section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Magnific pop
+
 $(document).ready(function () {
   $('.image-container').magnificPopup({
 	  delegate: 'a', // child items selector, by clicking on it popup will open
@@ -5,5 +7,37 @@ $(document).ready(function () {
 	  gallery: {
 	  	enabled: true
 	  }
+  })
+})
+
+// owl carousel
+$(document).ready(function () {
+  var owl = $('.owl-carousel')
+  owl.owlCarousel({
+			    items: 4,
+			    loop: true,
+			    nav: true,
+			    margin: 10,
+			    autoplay: true,
+			    autoplayTimeout: 2000,
+			    smartSpeed: 4000,
+			    autoplayHoverPause: true,
+			    responsive: {
+		        0: {
+		            items: 2
+		        },
+		        600: {
+		            items: 3
+		        },
+		        1000: {
+		            items: 3
+		        }
+		    }
+  })
+  $('.play').on('click', function () {
+			    owl.trigger('play.owl.autoplay', [1000])
+  })
+  $('.stop').on('click', function () {
+			    owl.trigger('stop.owl.autoplay')
   })
 })
